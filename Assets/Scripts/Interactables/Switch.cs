e702f6edc393637fe8ec7a2a8d1d84ceec3e3b37
Dannan Vazquez <dannanvazquez@gmail.com>
2023-02-15 11:23:42 -0800

    Minor adjustments
    Minor changes to positions of prefabs and indicator for when switch is on/off. Indicator is temporarily and will be changed later.

--- Assets/Scripts/Interactables/Switch.cs
+++ Assets/Scripts/Interactables/Switch.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 
 public class Switch : MonoBehaviour, IInteractable {
+    [SerializeField] private GameObject triggerObject;
+
     public bool isTriggered = false;
 
     public void OnInteract(PlayerInteractions interactee) {
         isTriggered = !isTriggered;
         if (isTriggered) {
-            Debug.Log($"{gameObject.name} is on!");
         } else {
-            Debug.Log($"{gameObject.name} is off!");
         }
+        triggerObject.SetActive(isTriggered);
     }
 }
