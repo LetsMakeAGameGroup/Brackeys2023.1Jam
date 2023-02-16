4cff93fbfce9a503c10ce173c96dd30c95aedfe8
Dannan Vazquez <dannanvazquez@gmail.com>
2023-02-16 06:14:26 -0800

    Day cycle settings for objects
    Added script to prefabs to be able to edit what days the object should be shown on.

---
+++ Assets/Scripts/GameFramework/DayObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayObject : MonoBehaviour {
+    public bool firstDay = true;
+    public bool secondDay = true;
+    public bool thirdDay = true;
+    public bool fourthDay = true;
+    public bool fifthDay = true;
+}
